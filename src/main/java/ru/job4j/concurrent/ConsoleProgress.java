7d6a07fc5f198cc14f72820f35f6d8d84e6ad374
ПК <slava_zen@mail.ru>
2024-06-14 14:38:16 +0500

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,31 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+        var process = new char[]{'-', '\\', '|', '/' };
+        int i = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\r load: " + process[i++]);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (i == process.length) {
+                i = 0;
+            }
+        }
+    }
+}
